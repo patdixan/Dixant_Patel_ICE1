74ed996f62c82c74c02e09e3ea25a8f789236ced
patdixan <patdixan@sheridancollege.ca>
2023-05-31 13:43:31 -0700

    Version 1

--- src/card/Card.java
+++ src/card/Card.java
@@ -5,20 +5,22 @@
 package card;
 
 /**
- * A class that models playing card Objects. Cards have 
- * a value (note that Ace = 1, Jack -11, Queen =12, King = 13)
- * A suit (clubs, hearts, spades, diamonds).
- * There are 52 cards in a deck, no jokers.
- * This code is to be used in ICE1. When you create your own branch,
- * add your name as a modifier.
+ * A class that models playing card Objects. Cards have a value (note that Ace =
+ * 1, Jack -11, Queen =12, King = 13) A suit (clubs, hearts, spades, diamonds).
+ * There are 52 cards in a deck, no jokers. This code is to be used in ICE1.
+ * When you create your own branch, add your name as a modifier.
+ *
  * @author srinivsi
+ * @modifier Dixantkumar Patel 
+ * Student Id - 991650706
  */
 public class Card {
 
-   private String suit; //clubs, spades, diamonds, hearts
-   private int value;//1-13
+    private String suit; //clubs, spades, diamonds, hearts
+    private int value;//1-13
 
-   public static final String [] SUITS = {"Hearts", "Diamonds", "Spades", "Clubs"};
+    public static final String[] SUITS = {"Hearts", "Diamonds", "Spades", "Clubs"};
+
     /**
      * @return the suit
      */
@@ -46,7 +48,5 @@ public class Card {
     public void setValue(int value) {
         this.value = value;
     }
-   
-   
-    
+
 }
--- src/card/CardTrick.java
+++ src/card/CardTrick.java
@@ -5,27 +5,62 @@
 package card;
 
 /**
- * A class that fills a magic hand of 7 cards with random Card Objects
- * and then asks the user to pick a card and searches the array of cards
- * for the match to the user's card. To be used as starting code in ICE 1
+ * A class that fills a magic hand of 7 cards with random Card Objects and then
+ * asks the user to pick a card and searches the array of cards for the match to
+ * the user's card. To be used as starting code in ICE 1
+ *
  * @author srinivsi
+ * @modifier Dixantkumar Patel 
+ * Student Id - 991650706
+ *
  */
+import java.util.Random;
+import java.util.Scanner;
+
 public class CardTrick {
-    
-    public static void main(String[] args)
-    {
+
+    public static void main(String[] args) {
+        //have to generate 7 random cards to stire in array magichand
         Card[] magicHand = new Card[7];
-        
-        for (int i=0; i<magicHand.length; i++)
-        {
-            Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+        Random input = new Random();
+        Scanner scanner = new Scanner(System.in);
+
+        for (int i = 0; i < magicHand.length; i++) {
+
+            Card c1 = new Card();
+
+            c1.setValue(input.nextInt(13) + 1); // 1 to 13
+            c1.setSuit(Card.SUITS[input.nextInt(4)]);
+
+            magicHand[i] = c1;
+
         }
-        
-        //insert code to ask the user for Card value and suit, create their card
-        // and search magicHand here
-        //Then report the result here
+
+        System.out.println("Pick a card between 0-13: ");
+        int value = scanner.nextInt();
+        System.out.println("Pick a card suit from Hearts, Diamonds, Spades and Clubs: ");
+        String suit = scanner.next();
+
+        Card userCard = new Card();
+        userCard.setValue(value);
+        userCard.setSuit(suit);
+
+        boolean found = false;
+        for (Card card : magicHand) {
+            if (card.equals(userCard)) {
+                found = true;
+                break;
+            }
+        }
+
+        if (found) {
+            System.out.println("Your card is in the magic hand!");
+        } else {
+            System.out.println("Your card is not in the magic hand.");
+        }
     }
-    
+
+    //insert code to ask the user for Card value and suit, create their card
+    // and search magicHand here
+    //Then report the result here
 }
